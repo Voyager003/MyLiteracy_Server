7f3ea479e054c7c884fd9445a98046146178cf31
voyager003 <miraclex000@gmail.com>
2024-04-21 21:16:05 +0900

    Initial commit

---
+++ src/main/java/com/myliteracy_server/myliteracyServerApplication.java
@@ -0,0 +1,12 @@
+package com.myliteracy_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class myliteracyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(myliteracyServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/myliteracy_server/MyliteracyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myliteracy_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyliteracyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
